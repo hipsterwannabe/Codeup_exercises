ac83a35ffd8d2a57a84884a0a4d1d24390d50b31
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-13 10:23:38 -0500

    added msgs for input errors

--- for.php
+++ for.php
@@ -6,6 +6,22 @@ $start = trim(fgets(STDIN));
 fwrite(STDOUT, "Now, give me an ending number.\n");
 $end = trim(fgets(STDIN));
 
+// check to see if entries are valid integers, are not equal, and are in correct order
+// error msgs if any errors
+if (!is_numeric($start) || !is_numeric($end)) {
+	echo "Please enter two integers.\n";
+	exit(1);
+} else {
+	if ($start == $end){
+		echo "Please enter two different integers.\n";
+		exit(1);
+	} else {
+		if ($end < $start);{
+			echo "Please make sure the second number is less than the first.\n";
+		}
+	}
+} 
+
 
 // display all numbers in between given numbers
 for ($i = $start; $i <= $end; $i++) { 
