f88783ddcb1772266561142155c499a5ec21391d
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-13 10:37:38 -0500

    Added incrementer, debugged error msgs

--- for.php
+++ for.php
@@ -6,6 +6,13 @@ $start = trim(fgets(STDIN));
 fwrite(STDOUT, "Now, give me an ending number.\n");
 $end = trim(fgets(STDIN));
 
+// prompt user for counting increment, default to 1 if none given
+fwrite(STDOUT, "How much do you want to count by?\n");
+$increment = trim(fgets(STDIN));
+if (!is_numeric($increment)){
+	$increment = 1;
+}
+
 // check to see if entries are valid integers, are not equal, and are in correct order
 // error msgs if any errors
 if (!is_numeric($start) || !is_numeric($end)) {
@@ -16,15 +23,16 @@ if (!is_numeric($start) || !is_numeric($end)) {
 		echo "Please enter two different integers.\n";
 		exit(1);
 	} else {
-		if ($end < $start);{
+		if ($end < $start){
 			echo "Please make sure the second number is less than the first.\n";
+			exit(1);
 		}
 	}
 } 
 
 
 // display all numbers in between given numbers
-for ($i = $start; $i <= $end; $i++) { 
+for ($i = $start; $i <= $end; $i = $i + $increment) { 
 	echo "$i\n";
 }
 
