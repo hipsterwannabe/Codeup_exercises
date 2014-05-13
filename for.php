0a14c473b9653a3eb08d0377e403a03852703eed
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-13 10:15:29 -0500

    debugged counter

--- for.php
+++ for.php
@@ -1,14 +1,15 @@
 <?php
 
 //prompt user for starting and ending number
-fwrite(STDOUT, "Hello, give me a beginning number."\n);
-$start = fgets(STDIN);
-fwrite(STDOUT, "Now, give me an ending number."\n);
-$end = fgets(STDIN);
+fwrite(STDOUT, "Hello, give me a beginning number.\n");
+$start = trim(fgets(STDIN));
+fwrite(STDOUT, "Now, give me an ending number.\n");
+$end = trim(fgets(STDIN));
 
-//display all numbers in between given numbers
-for ($i=$start; $i < $end; $i++) { 
-	echo {$i\n};
+
+// display all numbers in between given numbers
+for ($i = $start; $i <= $end; $i++) { 
+	echo "$i\n";
 }
 
 ?>
