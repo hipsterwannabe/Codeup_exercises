373ef741ef4cb225b578f380f05fa0c37bceeb6a
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-13 14:49:36 -0500

    Part 3 of exercise, printing all elements

--- foreach.php
+++ foreach.php
@@ -6,26 +6,36 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 // foreach ($things as $thing) {
 // 	if (is_int($thing)) {
-// 		echo "Integer\n";
+// 		echo "$thing is an Integer\n";
 // 	} elseif (is_float($thing)) {
-// 		echo "Float\n";
+// 		echo "$thing is a Float\n";
 // 	} elseif (is_bool($thing)) {
-// 		echo "Boolean\n";
+// 		echo "$thing is a Boolean\n";
 // 	} elseif (is_array($thing)) {
-// 		echo "Array\n";
+// 		echo "$thing is an Array\n";
 // 	} elseif (is_null($thing)) {
-// 		echo "Null\n";
+// 		echo "$thing is a Null\n";
 // 	} elseif (is_string($thing)) {
-// 		echo "String\n";
+// 		echo "$thing is a String\n";
 // 	}
 // }
 
 //Construct a loop that iterates through each value 
 //and outputs only values with a type that is scalar
 
+// foreach ($things as $thing) {
+// 	if (is_scalar($thing)) {
+// 		fwrite(STDOUT, $thing.PHP_EOL);
+// 	}
+// }
+
+//Create a loop that will echo out every value, including those nested in arrays.
+
 foreach ($things as $thing) {
-	if (is_scalar($thing)) {
-		fwrite(STDOUT, $thing.PHP_EOL);
+	if (is_array($thing)){
+		print_r($thing);
+	} else {
+	echo $thing.PHP_EOL;
 	}
 }
 
