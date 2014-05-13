9634564b3758eda2ab72261da9bacf127682c887
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-13 18:09:03 -0500

    initial fruit array assignment

---
+++ fruit.php
@@ -0,0 +1,17 @@
+<?php
+// Create array of fruits, write for and foreach loops that
+// prints names of fruits
+$fruits = array(apple, cherry, peach, lemon, lime);
+
+// for ($i = 0; $i < count($fruit); $i++){
+// 	echo $fruits[$i].PHP_EOL;
+// }
+
+foreach ($fruits as $fruit) {
+	echo $fruit.PHP_EOL;
+}
+
+
+
+
+?>
