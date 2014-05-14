6837468b4633645ad3e25cae20a57e3c7dbb8a49
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-14 11:11:58 -0500

    part 2 of exercise with break

--- break_continue.php
+++ break_continue.php
@@ -2,11 +2,20 @@
 
 //Create a for loop that shows all 
 //even numbers between 1 and 100 using continue.
+// for ($i = 1; $i <= 100; $i++){
+// 	if ($i % 2 != 0){
+// 		continue;
+// 	}
+// 	echo "$i\n";
+// } 
+
+//Create another for loop that counts from 1 to 100, 
+//but stops after 10 using break.
+
 for ($i = 1; $i <= 100; $i++){
-	if ($i % 2 != 0){
-		continue;
+	if ($i > 10){
+		break;
 	}
 	echo "$i\n";
-} 
-
+}
 ?>
