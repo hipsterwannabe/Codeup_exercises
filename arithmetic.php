09fe3cb450e624c54f4b880511de536012ff6ca2
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-15 09:53:28 -0500

    second part of exercise

--- arithmetic.php
+++ arithmetic.php
@@ -16,9 +16,14 @@ function divide($a, $b) {
     echo $a / $b.PHP_EOL;
 }
 
+function modulus($a, $b) {
+	echo $a % $b.PHP_EOL;
+}
+
 add(3,9);
 subtract(10,2.4);
-multiply(7,21);
-divide(100,10);
+multiply(7,6);
+divide(100,7);
+modulus(98,11);
 
 ?>
