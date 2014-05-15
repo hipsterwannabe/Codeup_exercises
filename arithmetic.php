6c9f470a542be21e3024fbd0e606ca406916310a
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-15 11:51:53 -0500

    Step 1 of part 2 in arithmetic exercise

--- arithmetic.php
+++ arithmetic.php
@@ -1,29 +1,59 @@
 <?php
 
-function add($a, $b) {
-    echo $a + $b.PHP_EOL;
-}
 
+// 1.Validate all the arguments, and display an error if the input is not numeric.
+
+// 2. Validate divide by 0 errors, display error if attempts to divide by 0 are made.
+
+// 3. Make the error messages show the values of the arguments.
+
+// 4. Refactor the error messages into their own function, 
+//    have the other functions use it for error messaging.
+
+
+
+
+function add($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+    	echo $a + $b.PHP_EOL;
+	} else {
+		echo '$a and $b should be numeric, yo!'.PHP_EOL;
+	}
+}
 function subtract($a, $b) {
-    echo $a - $b.PHP_EOL;
+    if (is_numeric($a) && is_numeric($b)){
+		echo $a - $b.PHP_EOL;
+	} else {
+		echo '$a and $b should be numeric, yo!'.PHP_EOL;
+	}
 }
 
 function multiply($a, $b) {
+    if (is_numeric($a) && is_numeric($b)){
     echo $a * $b.PHP_EOL;
+} else {
+	echo '$a and $b should be numeric, yo!'.PHP_EOL;
+}
 }
 
 function divide($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
     echo $a / $b.PHP_EOL;
+} else {
+	echo '$a and $b should be numeric, yo!'.PHP_EOL;
+}
 }
 
 function modulus($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
 	echo $a % $b.PHP_EOL;
+} else {
+	echo '$a and $b should be numeric, yo!'.PHP_EOL;
+}
 }
 
-add(3,9);
-subtract(10,2.4);
-multiply(7,6);
-divide(100,7);
-modulus(98,11);
-
-?>
+add(1, 'dog');
+subtract(10, 4);
+multiply(true, 'omar');
+divide(10, 3);
+modulus(null, 4);
