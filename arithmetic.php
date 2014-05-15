c35a6805c2e926b707900cd5b74c3ca4535556fa
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-15 09:43:09 -0500

    initial commit

---
+++ arithmetic.php
@@ -0,0 +1,21 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b;
+}
+
+function subtract($a, $b) {
+    // Add code here
+}
+
+function multiply($a, $b) {
+    // Add code here
+}
+
+function divide($a, $b) {
+    // Add code here
+}
+
+
+
+?>
