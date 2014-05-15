2c90a595ba529e6759b2133f820c9ceff4a78838
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-15 16:17:22 -0500

    Step 1 of Part 2 of exercise

--- arithmetic.php
+++ arithmetic.php
@@ -10,21 +10,30 @@
 // 4. Refactor the error messages into their own function, 
 //    have the other functions use it for error messaging.
 
+// PART 2
+
+// 1. Refactor each function to return the result, removing the echo.
+
+// 2. Validate divide by 0 errors, return FALSE if divide by 0 is attempted.
 
 function error($a, $b){
-	echo $a . ' and ' . $b  . ' should be numeric, yo!'.PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+		return true;
+	} else {
+		echo "Error! \$a and/or \$b was not a number!\n";
+		return false;
+	}
 }
 
 function add($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	echo $a + $b.PHP_EOL;
-	} else {
-		error($a, $b);
+	if (error($a, $b)) {
+		return $a + $b;
 	}
 }
+
 function subtract($a, $b) {
     if (is_numeric($a) && is_numeric($b)){
-		echo $a - $b.PHP_EOL;
+		return $a - $b . PHP_EOL;
 	} else {
 		error($a, $b);
 	}
@@ -32,33 +41,33 @@ function subtract($a, $b) {
 
 function multiply($a, $b) {
     if (is_numeric($a) && is_numeric($b)){
-    echo $a * $b.PHP_EOL;
-} else {
-	error($a, $b);
+    	return $a * $b . PHP_EOL;
+} 	else {
+		error($a, $b);
 }
 }
 
 function divide($a, $b) {
 	if ($b == 0){
-    echo 'You cannot divide by zero, yo!'.PHP_EOL;
-} elseif (is_numeric($a) && is_numeric($b)) {
-	echo $a / $b.PHP_EOL;
-} else {
- 	error($a, $b);
+    	return 'You cannot divide by zero, yo!' . PHP_EOL;
+} 	elseif (is_numeric($a) && is_numeric($b)) {
+		return $a / $b . PHP_EOL;
+} 	else {
+ 		error($a, $b);
 }
 }
 
 function modulus($a, $b) {
 	if (is_numeric($a) && is_numeric($b)){
-	echo $a % $b.PHP_EOL;
-} else {
-	error($a, $b);
+		return $a % $b . PHP_EOL;
+} 	else {
+		error($a, $b);
 }
 }
+ 
+echo add(1, 7);
+echo subtract(10, 4);
+echo multiply('banana', 14);
+echo divide(9, 0);
+echo modulus(13, 4);
 
-
-add(1, 7);
-subtract(10, 4);
-multiply('banana', 14);
-divide(0, 9);
-modulus(13, 4);
