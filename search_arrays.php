3122cae2b61cf5f1f8ce8ff4abc0c3974c2c4844
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-19 15:11:59 -0500

    refactored earlier code, completed step 3 of exercise

--- search_arrays.php
+++ search_arrays.php
@@ -12,17 +12,34 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 // Create a function to compare 2 arrays that returns the number of values in common 
 // between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
 
-function findName($name, $nameArray, $result = FALSE){
-	if (is_numeric(array_search($name, $nameArray))) {
-		$result = 'TRUE';
-	} else {
-		$result = 'FALSE';
+// STEP 2
+function findName($needle, $haystack){
+	if ((array_search($needle, $haystack)) === false) {
+		return false;
 	}
-	return $result;
+	else
+	{
+		return true;
+	}
 }
 
-echo findName('Tina', $names).PHP_EOL;
-echo findName('Tina', $compare).PHP_EOL;
+// echo findName('Tina', $names).PHP_EOL;
+// echo findName('Tina', $compare).PHP_EOL;
 
-echo findName('Bob', $names).PHP_EOL;
-echo findName('Bob', $compare).PHP_EOL;
+// echo findName('Bob', $names).PHP_EOL;
+// echo findName('Bob', $compare).PHP_EOL;
+
+// STEP 3
+function compareNames ($array1, $array2){
+	$count = 0;
+	foreach ($array1 as $value) {
+		if (findName($value, $array2)) {
+			$count += 1;
+		}
+	}
+	return $count;
+}
+
+
+
+echo compareNames($names, $compare).PHP_EOL;
