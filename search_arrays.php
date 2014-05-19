e6a713ed680e3273723c72fda282699b02649c46
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-19 13:54:52 -0500

    Step 2 of exercise

--- search_arrays.php
+++ search_arrays.php
@@ -11,3 +11,18 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 // Create a function to compare 2 arrays that returns the number of values in common 
 // between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
+
+function findName($name, $nameArray, $result = FALSE){
+	if (is_numeric(array_search($name, $nameArray))) {
+		$result = 'TRUE';
+	} else {
+		$result = 'FALSE';
+	}
+	return $result;
+}
+
+echo findName('Tina', $names).PHP_EOL;
+echo findName('Tina', $compare).PHP_EOL;
+
+echo findName('Bob', $names).PHP_EOL;
+echo findName('Bob', $compare).PHP_EOL;
